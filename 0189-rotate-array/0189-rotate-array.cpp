234c2e6f44995e5e9f9e5a89acb05e57543f9e2a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-04-21 13:38:24 +0530

    Time: 29 ms (78.72%), Space: 25 MB (61.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,40 +1,10 @@
-class Solution {
-public:
-    void display(vector<int>&n){
-        for(int i = 0;i < n.size();i++){
-            cout<<n[i]<<" ";
-        }
-    }
-    
-    
-    
-    
-    
-    void reversePart(int i ,int j,vector<int>&v){
-    while(i<j){
-        int temp = v[i];
-        v[i] = v[j];
-        v[j]=temp;
-        i++;
-        j--;
-    }
-}
-    
-    
-    
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-    
-        //if k >= n -> this will give index out of bound for the below code
-        if(k > n) k %=n;
-        //reversePart of Array
-         
-        reversePart(0,n-k-1,nums);
-        reversePart(n-k,n-1,nums);
-        reversePart(0,n-1,nums);
-        cout<<endl;
-        display(nums);
 
-        
+    class Solution {
+public:
+    void rotate(vector<int>& v, int k) {
+        int a =k%v.size();
+        reverse(v.begin(),v.end());
+        reverse(v.begin(),v.begin()+a);
+        reverse(v.begin()+a,v.end());
     }
 };
