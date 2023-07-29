f227deab0706bed8d52cc1ce48dd77cf7d2658b0
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-07-29 10:05:34 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (35.25%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector< vector<int> >v;
+        //creating a 2D vector containing vectors of variable sizes
+        for(int i = 1;i <= numRows;i++){
+            vector<int>a(i);
+            v.push_back(a);
+        }
+        //generate
+        for(int i = 0;i < numRows;i++){
+            for(int j = 0;j <= i ;j++){
+                if(j == 0 || j==i) {
+                    v[i][j]=1;
+                }
+                else {
+                    v[i][j] = v[i-1][j]+v[i-1][j-1];
+                }
+            }
+        }
+        
+        return v;
+        
+    }
+};
