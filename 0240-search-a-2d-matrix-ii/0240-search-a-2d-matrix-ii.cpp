e16b3da08311097f166622b249847aecd14db46e
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-07-30 07:20:28 +0530

    Time: 2523 ms (5.00%), Space: 14.9 MB (37.38%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,6 +3,7 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size();
         int cols = matrix[0].size();
+        //this is not an efficient algorithm
         for(int i = 0;i < rows;i++){
             for(int j = 0;j < cols;j++){
                 if(matrix[i][j]==target) return true;
