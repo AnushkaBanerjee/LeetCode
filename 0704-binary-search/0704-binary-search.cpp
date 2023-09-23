4f0559b8500241079d1aebf097d2ce23f076517d
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-09-23 18:48:52 +0530

    Time: 27 ms (67.37%), Space: 28 MB (7.67%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,11 +4,13 @@ public:
         int lo = 0;
         int hi = nums.size()-1;
         while(lo <= hi){
-            int mid = (hi+lo)/2;
+            // int mid = (hi+lo)/2;-->might go out of bound of integers
+            int mid = lo + (hi-lo)/2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]<target) lo = mid+1;
             else hi = mid-1;
         }
         return -1;
+        //hi = INT_MAX  and lo = INT_MAX-10
     }
 };
