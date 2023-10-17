567df324655b92185e502c0e8ccad6deafac7dac
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-10-17 19:43:49 +0530

    Time: 16 ms (46.89%), Space: 17.4 MB (27.23%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int c=0;
-        for(int i=0;i<n;i++)
-        {
-            c= c^nums[i];
+        int n = nums.size();
+        int singleOne = 0;
+        if(n == 1) return nums[0];
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<n;i+=2){
+            if(nums[i]!=nums[i+1]) {
+                singleOne = nums[i];
+                break;
+            }
         }
-            return c;
+        return singleOne;
     }
 };
