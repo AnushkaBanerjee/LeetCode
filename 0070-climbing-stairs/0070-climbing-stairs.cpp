766bc11831055a86786956a26bf8452f92c0309a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2023-11-01 18:11:36 +0530

    Time: 2 ms (46.93%), Space: 6.3 MB (72.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    
+    int climbStairs(int n) {
+       int n1 = 1;
+       int n2 = 2;
+       int sum = 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+       for(int i = 3;i <= n;i++)
+       {
+           sum = n2 + n1;
+           n1 = n2;
+           n2 = sum;
+       }
+       return sum;
+    }
+};
