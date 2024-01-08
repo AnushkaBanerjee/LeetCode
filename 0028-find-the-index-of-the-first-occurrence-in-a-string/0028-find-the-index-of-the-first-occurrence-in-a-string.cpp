958e8a0bc3a9f1036a5423310fe97023bcce6314
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-01-08 22:22:48 +0530

    Time: 3 ms (45.69%), Space: 6.5 MB (87.10%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,17 +3,17 @@ public:
     int strStr(string haystack, string needle) {
      int i = 0;
         int j = 0;
-        int idx = -1; // Initialize idx to -1
+        int idx = -1;
         while (i < haystack.size()) {
             if (haystack[i] == needle[j]) {
                 if (j == 0) {
-                    idx = i; // Update idx only on the first match
+                    idx = i; 
                 }
                 i++;
                 j++;
             } else {
                 if (j > 0) {
-                    i = idx + 1; // If mismatch after partial match, reset i to the next index of the last match
+                    i = idx + 1; 
                     j = 0;
                 } else {
                     i++;
@@ -21,10 +21,10 @@ public:
             }
 
             if (j == needle.size()) {
-                return idx; // Return the index of the first occurrence
+                return idx;
             }
         }
 
-        return -1; // Return -1 if needle is not part of haystack
+        return -1; 
     }
 };
