ee2c2fdaa45a7c59651a993b170155d9e263660e
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-02 19:22:06 +0530

    Time: 2 ms (39.80%), Space: 8.1 MB (34.19%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,7 +5,7 @@ public:
    
         while(x){
             int lastDigit = x%10;
-            if((revNum > INT_MAX/10) || (revNum < INT_MIN/10)) return 0;
+            if((revNum > INT_MAX/10) || (revNum < INT_MIN/10)) return 0;//res*10 might cause the operation to overflow the representable range of int
             revNum = (revNum * 10)+lastDigit;
              x = x/10;
             
