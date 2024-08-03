2acfacb7e53190ac2601bf885e3c7838772adb56
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-03 19:31:52 +0530

    Time: 4 ms (84.37%), Space: 8.5 MB (45.87%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-        int reverse(int x) {
-        
-        long long int t = x;
-        
-        long long int rev = 0;
-        while(t != 0){
-            // While t is not equal to 0, 
-            int a = t % 10; // store last digit of the t
-            rev = (rev * 10) + a; // and add a to the rev 
-            t = t/10;
-        }
-        // Finally we get reverse number
-        return rev;
-    }
-    
     bool isPalindrome(int x) {
-        // If negative number return false;
+        int revNum = 0;
+        int originalNum = x;
+        while(x > 0){
+            int lastDigit = x%10;
+            if(revNum > INT_MAX/10 || revNum < INT_MIN /10) return false;
+            revNum = (revNum * 10) + lastDigit;                
+            x = x / 10;
+        }
         if(x < 0){
             return false;
         }
         
-        int t = reverse(x);
-        // reverse it and compare with x if same return true else false
-        if(t == x){
+        if(revNum == originalNum){
             return true;
         }
         else{
             return false;
         }
     }
-    
 };
