ffd47665a9d4b9301cc0f7a129068047a1bfcef8
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-08 20:20:36 +0530

    Time: 8 ms (33.89%), Space: 8.7 MB (67.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
+   bool isPalindrome(string s) {
         int left = 0, right = s.length()-1;
         while(left<right)
         {
-            if(!isalnum(s[left])) 
+            if(!isalnum(s[left])) //The isalnum function checks whether a character is alphanumeric, meaning it is either a letter (uppercase or lowercase) or a digit.
                 left++;
             else if(!isalnum(s[right])) 
                 right--;
-            else if(tolower(s[left])!=tolower(s[right])) 
+            else if(tolower(s[left])!=tolower(s[right])) //This converts the character at the left index of the string s to lowercase, if it is an uppercase letter. It ensures the comparison between left and right characters is case-insensitive.
                 return false;
             else {
                 left++; 
