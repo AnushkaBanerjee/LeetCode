ab925681184c8ed3408436f7ff4fbad7b7b3ca1e
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-11 21:14:27 +0530

    Time: 3 ms (57.48%), Space: 11.5 MB (21.07%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map< int , int> mp; 
+        unordered_map< int , int> mp; 
         
         for(int &x : arr){// store frequencies of numbers in map 
             mp[x]++;
         }
-       set<int>st;
+       unordered_set<int>st;
         for(auto &it : mp){
             int freq = it.second; // it.second contains frequency & it.first contains number
             if(st.find(freq) != st.end()){
