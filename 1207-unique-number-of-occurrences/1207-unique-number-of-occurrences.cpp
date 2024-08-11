7b419e15acc93187e074eb47b9f78264f8350bf7
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-11 21:13:57 +0530

    Time: 6 ms (28.37%), Space: 11.2 MB (64.51%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,53 +1,22 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+        map< int , int> mp; 
         
-        //[1,2,2,10]
-        vector<int> freq;
-        int count = 1;
-        sort(arr.begin(),arr.end());
-        
-        
-        
-        if(arr.size() == 1){
-            return true;
+        for(int &x : arr){// store frequencies of numbers in map 
+            mp[x]++;
         }
-        
-        if(arr.size() == 2){
-            if(arr[0] != arr[1]) return false;
-            else return true;
-        }
-        
-        
-        for(int i = 0;i < arr.size()-1;i++){//0 1 2
-            
-            if(arr[i] == arr[i+1]){ //1!=2;2 == 2,2!=10,
-                count++;//1 2
+       set<int>st;
+        for(auto &it : mp){
+            int freq = it.second; // it.second contains frequency & it.first contains number
+            if(st.find(freq) != st.end()){
+                // we have found duplicate frequency in set
+                return false;
             }
-            else {
-                
-                freq.push_back(count);//freq = [1,2,
-                count = 1;
-            }
-            
+            st.insert(freq);// insert in set as previously it wasn't in set
         }
-        
-        
-        
-        if(count >= 1){
-            freq.push_back(count);
-        }
-        
-        if(freq.size() == 1){
-            return true;
-        }
-        
-        
-        sort(freq.begin(),freq.end());
-        for(int i = 0;i < freq.size()-1;i++){
-            if(freq[i]==freq[i+1]) return false;
-        }
-        
         return true;
+        
+        
     }
 };
