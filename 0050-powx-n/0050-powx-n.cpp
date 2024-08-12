38b689bf2b876b1328a6e83d55e1225e883f8ae4
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-12 21:49:39 +0530

    Time: 3 ms (38.99%), Space: 8.4 MB (9.80%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -15,8 +15,7 @@ double myPow(double x, int n)
 {
     if (x == 0 || x == 1)
         return x;
-    if (n == 0)
-        return 1.0;
+    
     if (x == -1)
     {
         return n % 2 == 0 ? 1.0 : -1.0;
