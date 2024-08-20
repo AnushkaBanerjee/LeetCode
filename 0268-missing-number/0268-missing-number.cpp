59ecb1db7ae346cf5fbb40d26e91f7ac95b4757e
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-20 19:31:01 +0530

    Time: 10 ms (84.48%), Space: 22.1 MB (8.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //brute force
-        
+        //better solution
         
         int n = nums.size();
         
+        vector<int>hash;
+        
         for(int i = 0;i <= n;i++){
-            int flag = 0;
-            for(int j = 0;j < n;j++){
-                if(nums[j] == i) {
-                    flag = 1;
-                    break;
-                }
+            hash.push_back(0);
+        }
+        
+        //increasing the hash values of elements present
+        
+        for(int i = 0;i < n;i++){
+            hash[nums[i]]++;
+        }
+        
+        //finding missing element
+        for (int i = 0; i <= n; i++) {
+         if (hash[i] == 0) {
+            return i;
             }
-            
-            if(flag == 0) return i;
-            
         }
         
         return -1;
+        
     }
 };
