2dda82d07c3fdb713bdf86da16456901ebc36313
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-21 13:16:02 +0530

    Time: 13 ms (64.56%), Space: 19.6 MB (28.46%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       map<long ,long> mpp;
         int n = nums.size();
-       for(int i = 0;i < n;i++ ){
-           mpp[nums[i]]++;
-       }
-        
-        for (auto it : mpp) {
-        if (it.second == 1)
-            return it.first;
-    }
-        return -1;
-        
+        int xorr = 0;
+        for(int i = 0;i < n;i++){
+            xorr = xorr ^ nums[i];
+            
+        }
+       return xorr;
     }
 };
