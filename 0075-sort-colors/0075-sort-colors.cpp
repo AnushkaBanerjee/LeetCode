de195546861ed2bb60d226e6708650c7acd0d6d4
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-23 23:07:04 +0530

    Time: 5 ms (18.13%), Space: 10.3 MB (10.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int noZ = 0,noO = 0,noT = 0;
+        //Using Dutch Flag algorithm
         int n = nums.size();
-        for(int i = 0;i < n;i++){
-            if(nums[i] == 0) noZ++;
-            if(nums[i] == 1) noO++;
-            if(nums[i]== 2) noT++;
+        int lo = 0,mid = 0,hi = n-1;
+        while(mid <= hi){
+            if(nums[mid] == 0){
+                swap(nums[mid] , nums[lo]);
+                lo++;
+                mid++;
+            }
+            
+            else if(nums[mid] == 1) mid++;
+            
+            else{
+               swap(nums[mid],nums[hi]);
+                hi--;
+            }
         }
-        
-        for(int i = 0;i < noZ;i++) nums[i] = 0;
-        for(int i = noZ;i < noZ+noO;i++) nums[i] = 1;
-        for(int i = noZ + noO;i < n;i++) nums[i] = 2;
     }
 };
