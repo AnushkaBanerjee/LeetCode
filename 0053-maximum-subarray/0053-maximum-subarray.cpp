227dfc3b27de795e4e86398965db0ff0317ac780
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-25 19:33:04 +0530

    Time: 87 ms (12.07%), Space: 70.6 MB (18.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,8 +4,8 @@ public:
         //kadande's algorithm
         int sum = 0;
         int maxi = INT_MIN;
-        for(auto it:nums){
-            sum += it;
+        for(int i = 0;i < nums.size();i++){
+            sum += nums[i];
             maxi = max(maxi,sum);
             if(sum < 0) sum = 0;
         }
