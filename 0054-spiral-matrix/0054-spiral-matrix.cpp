f4f935338485100fb76bde3ad64a73281f2c174a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-30 10:26:13 +0530

    Time: 3 ms (37.38%), Space: 8.5 MB (45.18%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int minr = 0,minc = 0;
-        int maxr = m-1,maxc = n-1;
-        int count = 0;
-        vector<int> v;
-       while(minr<=maxr && minc<=maxc){
-        //right
-        for(int j = minc;j <= maxc &&     count<m*n;j++){
-             v.push_back(matrix[minr][j]);
-            count++;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int right = m-1,left = 0,top = 0,bottom = n-1;
+        vector<int>ans;
+        while(left <= right && top <= bottom){
+            //printing from left to right
+            for(int i = left ;i <= right;i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            //printing top to bottom
+            for(int i = top ;i <= bottom;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            //printing from right to left
+            if(top <= bottom){
+            for(int i = right;i >=left;i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            }
+            //printing from bottom to top
+            if(left <= right){
+            for(int i = bottom ;i >= top;i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+            }
         }
+    return ans;
         
-        minr++;
-        // if(minr>maxr || minc>maxc)     break;
-         //down
-         for(int i = minr;i <= maxr &&     count<m*n;i++){
-            v.push_back(matrix[i][maxc]);
-            count++;
-         }
-        maxc--;
-        // if(minr>maxr || minc>maxc)     break;
-        //left
-        for(int j = maxc;j >=minc &&     count<m*n;j--){
-             v.push_back(matrix[maxr][j]);
-            count++;
-        }
-        maxr--;
-        // if(minr>maxr || minc>maxc)     break;
-        //top
-        for(int i = maxr;i >=minr &&     count<m*n;i--){
-             v.push_back(matrix[i][minc]);
-            count++;
-        }
-        minc++;
-        }
-        return v;
         }
     };
