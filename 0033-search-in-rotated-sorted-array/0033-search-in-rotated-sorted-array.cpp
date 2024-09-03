7a8361f9ce54fbaad694ec49e4ef2cebfe87dd21
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-09-03 16:41:49 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (71.00%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int k) {
         int n = nums.size();
-        int lo = 0;
-        int hi = n-1;
-        while(lo <= hi){
-            int mid = lo + (hi - lo)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid]<=nums[hi]){// mid - hi is sorted
-                if(target >= nums[mid] && target <= nums[hi]) lo = mid + 1;
-                else hi = mid - 1;
+        int low = 0,high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==k) return mid;
+            else if(nums[low] <= nums[mid]){
+                if (nums[low] <= k && k <= nums[mid]) {
+                //element exists:
+                high = mid - 1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
-            else{//lo-mid is sorted
-                
-                if(target >= nums[lo] && target<= nums[mid]) hi = mid - 1;
-                else lo = mid + 1;
-                
+            else{
+                if(nums[mid]<=k && k <= nums[high] ){
+                    //element exists:
+                low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
+        
         return -1;
     }
 };
