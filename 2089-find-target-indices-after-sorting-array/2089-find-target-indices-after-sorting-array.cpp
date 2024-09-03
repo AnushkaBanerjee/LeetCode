e357efdbb507ce1ac90adeec0fa572305766c2a1
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-09-03 11:18:59 +0530

    Time: 9 ms (13.50%), Space: 14.8 MB (9.90%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+      //lower bound
+    int lowerBound(vector<int> arr, int x,int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1; // look on the right
+            }
+        }
+       
+        return ans;
+    }
+    
+    //upper boound
+    int upperBound(vector<int> &arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // maybe an answer
+            if (arr[mid] > x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>v;
+         int n = nums.size();
+        int lb = lowerBound(nums,target,n);
+        if(lb == n || nums[lb]!= target) return {};
+        int ub = upperBound(nums,target,n);
+        if((ub-1) == lb) return {lb};
+        for(int i = lb;i <= ub-1;i++){
+            v.push_back(i);
+        }
+        return v;
+        
+    }
+};
