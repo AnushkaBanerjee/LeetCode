97cdc7979ac5444c60a9f177a0999f44316745ae
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-10-15 19:11:35 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (78.30%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        int lo = 0;
-        int hi = m*n-1;
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            
-            int row = mid/n;
-            int col = mid%n;
-            
-            if(matrix[row][col] == target) return true;
-            else if(matrix[row][col] > target) hi = mid-1;
-            else lo = mid+1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = 0,high = n*m-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int row = mid/m,col = mid%m;
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]<target)low = mid+1;
+            else high = mid-1;
         }
-       return false;
+        return false;
     }
-};//The above solution has T.C O(log(m*n))
+};
