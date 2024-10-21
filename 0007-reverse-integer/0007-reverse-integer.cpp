a2afb0eefa4be1d47d189c4f06942ce0f3582f9a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-10-21 20:07:43 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (79.65%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int reverse(int x) {
        int revNum = 0;
+       int lastDigit = 0;
    
         while(x){
-            int lastDigit = x%10;
+            lastDigit = x%10;
             if((revNum > INT_MAX/10) || (revNum < INT_MIN/10)) return 0;//res*10 might cause the operation to overflow the representable range of int
             revNum = (revNum * 10)+lastDigit;
              x = x/10;
