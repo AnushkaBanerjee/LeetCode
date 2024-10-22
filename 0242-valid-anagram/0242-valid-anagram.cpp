1ed52de2cb69c3fd0b4c630acb3e14bc2b20c060
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-10-22 20:07:17 +0530

    Time: 5 ms (80.91%), Space: 8.7 MB (93.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,7 +3,6 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        cout<<s<<endl<<t<<endl;
         if(s==t)return true;
         else return false;
     }
