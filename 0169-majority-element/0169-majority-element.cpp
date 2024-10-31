4d4fb2538e1d53f436ddc67ed6e7a9cf1e3b0e9e
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-10-31 08:30:43 +0530

    Time: 8 ms (13.97%), Space: 28 MB (25.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       //moore's voting algorithm
-        int count = 0 ;
-        int el;
-        
-        for(int i = 0;i < nums.size();i++){
-            if(count == 0 ){
-                el = nums[i];
-                count ++;
-            }
-            else if(el == nums[i]) count++;
-            else count--;
-        }
-        
-        return el;
+        sort(nums.begin(),nums.end());
+    return nums[(nums.size()/2)];
     }
 };
