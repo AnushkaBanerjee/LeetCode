1e7413e15c15c0ebc229b0648364bdbd7f1e069a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-02 07:52:58 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (83.84%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool check(vector<int>& arr) {
-        
-        
         int count = 0;
         int n = arr.size();
-        
-        //check whether a part of the array is unsorted 
-        //if arr[i] > arr[i+1] occurs more than once it means array is unsorted both before and after the largest element
         for(int i = 0;i < n - 1;i++){
             if(arr[i] > arr[i+1]) count++;
         }
-        
-        //it is for the condition where the parts of the array after and before the largest element is sorted but the array as a whole is not sorted
         if(arr[n-1] > arr[0]) count ++;
         
         if(count > 1) return false;
