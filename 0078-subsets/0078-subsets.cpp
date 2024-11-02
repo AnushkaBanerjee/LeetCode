4db3adcaa0cca7adb8a35113cdb6dc9ad505c932
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-02 12:39:06 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (74.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    
-   void  helper(vector<int>& nums,int idx,vector<vector<int>>&v,vector<int>temp)
-      
-    {
-        if(idx == nums.size()){
-            v.push_back(temp);
-            return;
-        }
-     
-        helper(nums,idx+1,v,temp);//not pick
-        temp.push_back(nums[idx]);
-        helper(nums,idx+1,v,temp);//one line missing
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        //we need tp store the vectors in a vector
-        vector<int> ans;
-        vector<vector<int>> finalAns;
-        helper(nums,0,finalAns,ans);
-        return finalAns;
-        
+        int n = nums.size();
+        vector<vector<int>> powSet;
+        for (int num = 0; num < (1 << n); num++){
+            vector<int>subset;
+            for(int i = 0;i < n;i++){
+                if(num & (1 << i))
+                    subset.push_back(nums[i]);
+            }
+            powSet.push_back(subset);
+        }
+        return powSet;
     }
 };
