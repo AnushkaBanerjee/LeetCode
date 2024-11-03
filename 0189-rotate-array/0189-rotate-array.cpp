d005f1344f3fef7e7c5c317f6d0e869f663556e6
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-03 07:56:08 +0530

    Time: 0 ms (100.00%), Space: 27.6 MB (81.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,27 +2,13 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        //the number of rotations if k > n as well as normal cases will be
         k = k % n;
-        
-        vector<int>temp;
-        //take a temporary array for storing the elements to be rotated
-        for(int i = n - k; i < n;i++){
-            temp.push_back(nums[i]);
-        }
-        
-        //now shift the non-rotated part to the right
-        
-        for(int i = n - k - 1;i >=0 ;i--){
-          nums[i + k] = nums[i];
-        }
-        
-        //putting temp in the array
-        for(int i = 0;i < k;i++){
-            nums[i] = temp[i];
-        }
-        
-        //time complexity : O(k) + O(n-k) + O(k) = O(n+k)
-        
+        if(k > n) return;
+        //reverse k elements from end
+        reverse(nums.end()-k,nums.end());
+        //reverse n-k elements from beginning
+        reverse(nums.begin(),nums.end()-k);
+        //reverse the entire array
+        reverse(nums.begin(),nums.end());
     }
 };
