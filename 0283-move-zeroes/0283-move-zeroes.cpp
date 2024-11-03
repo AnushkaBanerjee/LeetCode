b7bf9f26ccfb757e5a74d27272fe5ec10bc7fdc4
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-03 08:25:24 +0530

    Time: 0 ms (100.00%), Space: 21.8 MB (89.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         //optimal approach using two pointers
-        
-        int n = nums.size();
-        
         int j = -1;
-        
-        
-        //find the first occurence if 0 in the array
+        //take size of the array
+        int n = nums.size();
+        //check where the first zero lies in the array and point j to it
         for(int i = 0;i < n;i++){
-            if(nums[i] == 0) {
+            if(nums[i]==0){
                 j = i;
                 break;
             }
         }
+        //check if there are no zeros
         if(j == -1) return;
-        
-        for(int i = j+1;i < n;i++){
-            if(nums[i] != 0){
-                swap(nums[i],nums[j]);
+        //point i to the next element of j and swap them 
+        for(int i  = j+1; i < n;i++){
+            if(nums[i]!= 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                //increment position of j
                 j++;
             }
+            
         }
     }
 };
