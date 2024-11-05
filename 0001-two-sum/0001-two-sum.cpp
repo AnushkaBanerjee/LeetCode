81a3b3efdf3c555b2bb7a32d1eaff754adca710d
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-05 07:23:38 +0530

    Time: 3 ms (79.15%), Space: 14.6 MB (14.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //complexity : O(n log n)
+        map<int,int>mpp;
         int n = nums.size();
-        map <int,int> mpp;
         for(int i = 0;i < n;i++){
             int moreNeeded = target - nums[i];
-            if(mpp.find(moreNeeded) != mpp.end()){
+            if(mpp.find(moreNeeded)!=mpp.end()){
                 return {mpp[moreNeeded],i};
             }
-             mpp[nums[i]] = i;
+            mpp[nums[i]] = i;
         }
-       
         return {-1,-1};
     }
 };
