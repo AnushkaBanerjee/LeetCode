8338542f3186b7e94a8fd93dcfb02dffaa734dd3
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-20 08:22:38 +0530

    Time: 0 ms (100.00%), Space: 16.9 MB (57.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int>rows(n,0);
-        vector<int>cols(m,0);
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        //take extra space for tracking
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+        
+        //traverse matrix and set the elements at extra space to 1 if '0' is encountered
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
                 if(matrix[i][j] == 0){
-                    rows[i] = 1;
-                    cols[j] = 1;
+                    //set row element to 1
+                    row[i] = 1;
+                    //set col elemeny to 1
+                    col[j] = 1;
                 }
             }
         }
-        // Finally, mark all (i, j) as 0
-    // if row[i] or col[j] is marked with 1.
-         for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (rows[i] || cols[j]) {
-                matrix[i][j] = 0;
+        
+        //traverse again and change the matrix elements
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(row[i]||col[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
-    }
-
-    return;
-        
     }
 };
