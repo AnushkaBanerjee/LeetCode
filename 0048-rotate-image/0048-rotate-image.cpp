324d1b296cc174fdb9c58322a13c9a6eb768d1c9
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-30 19:10:45 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (15.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        int m = matrix[0].size();
         //transposing the matrix
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < i;j++){
+        for(int i = 0;i < n-1;i++){
+            for(int j = i+1;j <n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
@@ -14,3 +15,4 @@ public:
         }
     }
 };
+//time complexity : O(n/2*n/2)
